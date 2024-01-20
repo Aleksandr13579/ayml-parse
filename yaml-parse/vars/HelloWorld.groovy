1aa5cb1badcfb3376bb5cd461c9edaa4d37457bf
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-21 00:12:44 +0300

    shared libraries test

--- yaml-parse/vars/HelloWorld.groovy
+++ yaml-parse/vars/HelloWorld.groovy
@@ -1,14 +1,13 @@
 import main.groovy.org.example.classes.YamlFile
 
 def call(def jenkins) {
-    jenkins.sh "unzip ${params.ARCHIVE_1}"
     jenkins.echo "Hello world"
     jenkins.sh "pwd"
     jenkins.sh "ls -alrt"
     println("hello World!!")
 
-    YamlFile yamlFileFirst = new YamlFile("${params.ARCHIVE_1}")
-    YamlFile yamlFileSecond = new YamlFile("${params.ARCHIVE_2}")
+    YamlFile yamlFileFirst = new YamlFile("./resources/${params.ARCHIVE_1}")
+    YamlFile yamlFileSecond = new YamlFile("./resources/${params.ARCHIVE_2}")
     jenkins.echo "yamlFileFirst"
     yamlFileFirst.getData()
     jenkins.echo "yamlFileSecond"
