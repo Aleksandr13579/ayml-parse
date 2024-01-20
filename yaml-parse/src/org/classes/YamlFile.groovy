28491e313ed0df3481404d327b0c76f1e0e38654
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-20 23:13:32 +0300

    shared libraries test

---
+++ yaml-parse/src/org/classes/YamlFile.groovy
@@ -0,0 +1,46 @@
+package org.example.classes
+
+import org.yaml.snakeyaml.Yaml
+
+class YamlFile {
+
+    YamlFile(File yourFile) {
+        this.file = yourFile
+        this.fileName = "undefind"
+
+        try {
+            InputStream input = new FileInputStream(this.file)
+            this.yamlFile = new Yaml()
+            this.data = yamlFile.loadAll(input) as LinkedHashMap<String, String>
+        }
+        catch (Exception e) {
+            println(e)
+        }
+    }
+
+    YamlFile(String nameFile) {
+        try {
+            this.file = new File(nameFile)
+            this.fileName = nameFile
+            InputStream input = new FileInputStream(this.file)
+            this.yamlFile = new Yaml()
+            this.data = yamlFile.loadAll(input) as LinkedHashMap<String, String>
+        }
+        catch (FileNotFoundException e) {
+            println("File not found Exeption: " + e)
+        }
+    }
+
+     LinkedHashMap<String, String> getData() {
+        return this.data
+    }
+
+    boolean equals(YamlFile ya) {
+        return this.data == ya.getData()
+    }
+
+    private File file
+    private String fileName
+    private Yaml yamlFile
+    private LinkedHashMap<String, String> data
+}
