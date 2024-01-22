566270813298465c9b7057bdbdeb7342b31e1f0d
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-23 00:27:35 +0300

    shared libraries test

--- yaml-parse/vars/HelloWorld.groovy
+++ yaml-parse/vars/HelloWorld.groovy
@@ -3,19 +3,23 @@ import main.groovy.org.example.classes.Compare
 
 def call(def jenkins) {
     node {
-        stage('Exapmle') {
+        timestamps {
+            ansiColor("xterm") {
+                stage('Exapmle') {
 
-            YamlFile yamlFileFirst = new YamlFile("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_1}")
-            YamlFile yamlFileSecond = new YamlFile("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_2}")
+                    YamlFile yamlFileFirst = new YamlFile("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_1}")
+                    YamlFile yamlFileSecond = new YamlFile("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_2}")
 
-            Compare compare = new Compare(yamlFileFirst, yamlFileSecond)
-            jenkins.echo "Compare initialisation"
+                    Compare compare = new Compare(yamlFileFirst, yamlFileSecond)
+                    jenkins.echo "Compare initialisation"
 
-            println(compare.comprasion())
+                    println(compare.comprasion())
 
 
-            echo "============"
-            println(yamlFileFirst.equals(yamlFileSecond))
+                    echo "============"
+                    println(yamlFileFirst.equals(yamlFileSecond))
+                }
+            }
         }
     }
 }
