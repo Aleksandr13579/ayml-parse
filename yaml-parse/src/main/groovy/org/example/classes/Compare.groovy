85c27627afb0530f88fd39546039f4d1279e0471
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-27 22:57:38 +0300

    add metod converter to class compare

--- yaml-parse/src/main/groovy/org/example/classes/Compare.groovy
+++ yaml-parse/src/main/groovy/org/example/classes/Compare.groovy
@@ -62,8 +62,7 @@ class Compare {
      */
     @NonCPS
     private LinkedHashMap<String, String> converter(Map<String, ?> yam, String oldKey = "") {
-        LinkedHashMap<String, String> data = new LinkedHashMap<>()
-        jenkins.echo "vsdvdsvsdvsdvsdvsdvsdvsdvsd"
+        Map<String, String> data = new LinkedHashMap<>()
         yam.each { key, value ->
             if (value instanceof Map) {
                 if (oldKey != "") {
