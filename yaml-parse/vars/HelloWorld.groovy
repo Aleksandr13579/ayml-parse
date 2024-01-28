e0424a68be72cd0e35df7204d9245de47a793d2c
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-28 19:36:12 +0300

    add metod converter to class compare

--- yaml-parse/vars/HelloWorld.groovy
+++ yaml-parse/vars/HelloWorld.groovy
@@ -1,24 +1,19 @@
 import main.groovy.org.example.classes.YamlFile
 import main.groovy.org.example.classes.Compare
-import main.groovy.org.example.classes.Echo
 
 def call(def jenkins) {
-    new Echo(jenkins)
-    YamlFile yamlFileFirst
-    YamlFile yamlFileSecond
-    Compare compare
 
     node {
         timestamps {
             stage('Load filed') {
 
-                yamlFileFirst = new YamlFile()
+                YamlFile yamlFileFirst = new YamlFile()
                 yamlFileFirst.load("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_1}")
                 jenkins.echo  "vsdsdvsddsv"
-                yamlFileSecond = new YamlFile()
+                YamlFile yamlFileSecond = new YamlFile()
                 yamlFileSecond.load("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_2}")
                 jenkins.echo "vsdvsdvsdvs x xcxcxczzxc"
-                compare = new Compare(yamlFileFirst, yamlFileSecond)
+                Compare compare = new Compare(yamlFileFirst, yamlFileSecond)
                 def changes = compare.whatHasBeenAdded()
                 jenkins.echo "${changes}"
                 jenkins.echo "yyyyyyyy"
