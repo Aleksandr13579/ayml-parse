db5652934dc7b06c8930103e1f0c47b58ba57c08
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-29 00:46:52 +0300

    backup to scripted pipeline

--- yaml-parse/vars/HelloWorld.groovy
+++ yaml-parse/vars/HelloWorld.groovy
@@ -1,41 +1,47 @@
 import main.groovy.org.example.classes.YamlFile
 import main.groovy.org.example.classes.Compare
 
-LinkedHashSet<String> filesInFirstArchive = new LinkedHashSet<>()
+LinkedHashSet<String> filesInFirstArchive =  new LinkedHashSet<>()
 LinkedHashSet<String> filesInSecondArchive = new LinkedHashSet<>()
 
 def call(def jenkins) {
 
-    stage('Chekout') {
-        git(
-                url: 'https://github.com/Aleksandr13579/ayml-parse.git',
-                branch: "main"
-        )
-    }
-    stage('Unzip files') {
-        sh "mkdir ${env.WORKSPACE}/yaml-parse/resources/first"
-        sh "mkdir ${env.WORKSPACE}/yaml-parse/resources/second"
-        sh " unzip ${env.WORKSPACE}/yaml-parse/resources/first.zip -d ${env.WORKSPACE}/yaml-parse/resources/first"
-        sh " unzip ${env.WORKSPACE}/yaml-parse/resources/second.zip -d ${env.WORKSPACE}/yaml-parse/resources/second"
+    node {
+        timestamps {
+            try {
+                stage('Chekout') {
+                    git(
+                            url: 'https://github.com/Aleksandr13579/ayml-parse.git',
+                            branch: "main"
+                    )
+                }
+                stage('Unzip files') {
+                    sh "mkdir ${env.WORKSPACE}/yaml-parse/resources/first"
+                    sh "mkdir ${env.WORKSPACE}/yaml-parse/resources/second"
+                    sh " unzip ${env.WORKSPACE}/yaml-parse/resources/first.zip -d ${env.WORKSPACE}/yaml-parse/resources/first"
+                    sh " unzip ${env.WORKSPACE}/yaml-parse/resources/second.zip -d ${env.WORKSPACE}/yaml-parse/resources/second"
 
-        sh "ls -alrt ${env.WORKSPACE}/yaml-parse/resources"
-    }
-    stage('Load filed') {
-        script {
-            YamlFile yamlFileFirst = new YamlFile()
-            yamlFileFirst.load("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_1}")
+                    sh "ls -alrt ${env.WORKSPACE}/yaml-parse/resources"
+                }
+                stage('Load filed') {
 
-            YamlFile yamlFileSecond = new YamlFile()
-            yamlFileSecond.load("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_2}")
+                    YamlFile yamlFileFirst = new YamlFile()
+                    yamlFileFirst.load("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_1}")
 
-            Compare compare = new Compare(yamlFileFirst, yamlFileSecond)
-            def changes = compare.whatHasBeenAdded()
-            jenkins.echo "${changes}"
-        }
-    }
-    post {
-        always {
-            deleteDir()
+                    YamlFile yamlFileSecond = new YamlFile()
+                    yamlFileSecond.load("${env.WORKSPACE}/yaml-parse/resources/${params.ARCHIVE_2}")
+
+                    Compare compare = new Compare(yamlFileFirst, yamlFileSecond)
+                    def changes = compare.whatHasBeenAdded()
+                    jenkins.echo "${changes}"
+
+
+                }
+            } catch (exeption) {
+                throw exeption
+            } finally {
+                cleanWs()
+            }
         }
     }
 }
