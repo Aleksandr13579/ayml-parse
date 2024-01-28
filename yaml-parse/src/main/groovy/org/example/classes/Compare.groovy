ea6e2ccafe103b7a9bbffb2df8df8c00ae73699d
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-28 22:28:21 +0300

    add metod converter to class compare

--- yaml-parse/src/main/groovy/org/example/classes/Compare.groovy
+++ yaml-parse/src/main/groovy/org/example/classes/Compare.groovy
@@ -5,44 +5,43 @@ class Compare {
 
     LinkedHashMap<String, String> mismatchedFields
 
-    Compare(YamlFile first, YamlFile second, def jenkins) {
+    Compare(YamlFile first, YamlFile second) {
         this.firstYaml = first
         this.secondYaml = second
 
         this.dataFromFirstFile = new LinkedHashMap<>()
-        converter(jenkins,firstYaml.getData(), this.dataFromFirstFile)
+        converter(firstYaml.getData(), this.dataFromFirstFile)
 
         this.dataFromSecondFile = new LinkedHashMap<>()
-        converter(jenkins, secondYaml.getData(), this.dataFromSecondFile)
+        converter(secondYaml.getData(), this.dataFromSecondFile)
 
     }
 
-    Compare(File first, File second, def jenkins) {
+    Compare(File first, File second) {
         this.firstYaml = new YamlFile()
         firstYaml.load(first)
         this.secondYaml = new YamlFile()
         secondYaml.load(second)
 
         this.dataFromFirstFile = new LinkedHashMap<>()
-        converter(jenkins,firstYaml.getData(), this.dataFromFirstFile)
+        converter(firstYaml.getData(), this.dataFromFirstFile)
 
         this.dataFromSecondFile = new LinkedHashMap<>()
-        converter(jenkins, secondYaml.getData(), this.dataFromSecondFile)
+        converter(secondYaml.getData(), this.dataFromSecondFile)
 
     }
 
-    Compare(String first, String second, def jenkins) {
+    Compare(String first, String second) {
         this.firstYaml = new YamlFile()
         firstYaml.load(first)
         this.secondYaml = new YamlFile()
         secondYaml.load(second)
 
         this.dataFromFirstFile = new LinkedHashMap<>()
-        converter(jenkins,firstYaml.getData(), this.dataFromFirstFile)
-
+        converter(firstYaml.getData(), this.dataFromFirstFile)
 
         this.dataFromSecondFile = new LinkedHashMap<>()
-        converter(jenkins, secondYaml.getData(), this.dataFromSecondFile)
+        converter(secondYaml.getData(), this.dataFromSecondFile)
 
     }
 
@@ -59,33 +58,28 @@ class Compare {
      * @return
      */
     @NonCPS
-    private void converter(def jenkins, Map<String, ?> yam, Map<String, String> data, String oldKey = "") {
+    private void converter(Map<String, ?> yam, Map<String, String> data, String oldKey = "") {
         yam.each { key, value ->
             if (key == null) key = "2222222222"
             if (value == null) value = "2222222222"
             if (value instanceof Map) {
                 if (oldKey != "") {
-                    converter(jenkins,value, data, "${oldKey}.${key}")
+                    converter(value, data, "${oldKey}.${key}")
                 } else {
-                    converter(jenkins, value, data, "${key}")
+                    converter(value, data, "${key}")
                 }
             } else if (value instanceof ArrayList) {
                 value.each {
-                    jenkins.echo "${it.getClass().getSimpleName();}"
                     if (it instanceof String) {
-
-                        jenkins.echo "${oldKey}.${key} : ${value.toString()}"
                         data.put(oldKey + "." + key, value.toString())
                     } else {
-                        converter(jenkins, it, data, "${oldKey}.${key}")
+                        converter(it, data, "${oldKey}.${key}")
                     }
                 }
             } else {
                 if (oldKey != "") {
-                    jenkins.echo "${oldKey}.${key} : ${value.toString()}"
                     data.put(oldKey + "." + key, value.toString())
                 } else {
-                    jenkins.echo "${key} : ${value.toString()}"
                     data.put(key, value.toString())
                 }
             }
@@ -93,26 +87,22 @@ class Compare {
     }
 
     @NonCPS
-    String whatHasBeenAdded(def jenkins) {
+    String whatHasBeenAdded() {
         LinkedHashMap<String, String> differentValue = new LinkedHashMap<>()
         StringBuilder differentKey = new StringBuilder()
         StringBuilder allChanges = new StringBuilder()
         this.dataFromFirstFile.each {key,value ->
             if (this.dataFromSecondFile.containsKey(key)) {
-                jenkins.echo "2222222222"
                 if (value != this.dataFromSecondFile.get(key)) {
-                    jenkins.echo "${key} : ${value.toString()}"
-                    jenkins.echo "${this.dataFromSecondFile.get(key)}"
                     differentValue.put(key, this.dataFromSecondFile.get(key))
                 }
 
             } else {
-                jenkins.echo "${key}"
                 differentKey.append(key + ', ')
             }
         }
         allChanges.append("Различия по ключам: ${differentKey.length() != 0 ? differentKey : "Различий нет"} \n")
-                .append("Отличия по значениям \n")
+                  .append("Отличия по значениям \n")
         if (!differentValue.isEmpty()) {
             differentValue.each {key, value ->
                 allChanges.append(key + " : " + value + '\n')
