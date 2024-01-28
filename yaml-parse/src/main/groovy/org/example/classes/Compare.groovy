cc8fd6ebbe25413333de32d4a210ba95821bdbb6
Aleksandr13579 <al13579qw@yandex.ru>
2024-01-28 21:15:50 +0300

    add metod converter to class compare

--- yaml-parse/src/main/groovy/org/example/classes/Compare.groovy
+++ yaml-parse/src/main/groovy/org/example/classes/Compare.groovy
@@ -37,10 +37,10 @@ class Compare {
         this.secondYaml = new YamlFile()
         secondYaml.load(second)
 
-        this.dataFromFirstFile = new LinkedHashMap<>()
+        this.dataFromFirstFile = new TreeMap<>()
         converter(jenkins,firstYaml.getData(), this.dataFromFirstFile)
 
-        this.dataFromSecondFile = new LinkedHashMap<>()
+        this.dataFromSecondFile = new TreeMap<>()
         converter(jenkins, secondYaml.getData(), this.dataFromSecondFile)
 
     }
@@ -117,6 +117,6 @@ class Compare {
 
     private YamlFile firstYaml
     private YamlFile secondYaml
-    private LinkedHashMap<String, String> dataFromFirstFile
-    private LinkedHashMap<String, String> dataFromSecondFile
+    private TreeMap<String, String> dataFromFirstFile
+    private TreeMap<String, String> dataFromSecondFile
 }
