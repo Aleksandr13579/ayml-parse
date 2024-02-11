32b72801f1da239df3dcc39a88be6ae7ee02ada7
Aleksandr13579 <al13579qw@yandex.ru>
2024-02-11 21:50:06 +0300

    convert html to PDF

--- yaml-parse/src/main/groovy/org/example/classes/PDFConverter.groovy
+++ yaml-parse/src/main/groovy/org/example/classes/PDFConverter.groovy
@@ -1,15 +1,10 @@
 package main.groovy.org.example.classes
 
-import com.itextpdf.html2pdf.ConverterProperties
 import com.itextpdf.html2pdf.HtmlConverter
 import com.itextpdf.io.source.ByteArrayOutputStream
 import com.itextpdf.kernel.geom.PageSize
-import com.itextpdf.kernel.pdf.PdfVersion
 import com.itextpdf.kernel.pdf.PdfWriter
 import com.itextpdf.kernel.pdf.PdfDocument
-import com.itextpdf.layout.Document
-
-import javax.swing.text.html.HTMLDocument
 
 class PDFConverter {
 
