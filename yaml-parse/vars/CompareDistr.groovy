962793e9bda7aa887b073f95b10db6f56d006204
Aleksandr13579 <al13579qw@yandex.ru>
2024-02-14 00:55:33 +0300

    convert html to PDF

--- yaml-parse/vars/CompareDistr.groovy
+++ yaml-parse/vars/CompareDistr.groovy
@@ -34,6 +34,7 @@ def call() {
 
                     report.append("""
                             <table>
+                            <colgroup><col style="background:#d1d1d1"></colgroup>
                             <tr><th>текст заголовка</th><th>текст заголовка</th></tr>
                             <tr><td>данные</td><td>данные</td></tr>
                             </table><br><br>
